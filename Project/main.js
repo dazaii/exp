3248ddd81a39c8522fd7d9bfc3195a5a48c44d79
unknown <dazaii>
2021-09-30 09:22:17 +0800

    removed alert

--- Project/main.js
+++ Project/main.js
@@ -5,12 +5,10 @@ function change (){
     var statusLabel = document.getElementById ('status');
     if(tracker == 'm'){
         sound.src ="pic/a.png";
-        alert('Allow to listen?');
         statusLabel.innerHTML = "...Listening...";
         tracker = 'a';
     }else{
         sound.src="pic/m.png";
-        alert('Stop listening?');
         statusLabel.innerHTML = "Press to start";
         tracker = 'm';
     }
