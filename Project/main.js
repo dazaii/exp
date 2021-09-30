699decb640396335e7bf6fe166a09f94fe2dfdc0
unknown <dazaii>
2021-09-30 09:19:22 +0800

    added font

--- Project/main.js
+++ Project/main.js
@@ -2,13 +2,16 @@ let tracker ='m';
 
 function change (){
     var sound = document.getElementById ('mic');
+    var statusLabel = document.getElementById ('status');
     if(tracker == 'm'){
         sound.src ="pic/a.png";
         alert('Allow to listen?');
+        statusLabel.innerHTML = "...Listening...";
         tracker = 'a';
     }else{
         sound.src="pic/m.png";
         alert('Stop listening?');
+        statusLabel.innerHTML = "Press to start";
         tracker = 'm';
     }
 }
